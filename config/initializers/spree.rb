6b9cff78dbf9bacf3e4ebc387c855c2f03a0bfa6
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2018-01-04 03:19:48 -0800

    added logo

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -13,6 +13,8 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
+  config.logo = 'logo.png'
+  config.admin_interface_logo = 'logo.png'
 end
 
 Spree.user_class = "Spree::User"
